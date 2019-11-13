a23507b48c2f8b53d3484749abedc5de110ef0f5
Zarols <giovanni_zarola@libero.it>
2019-11-13 17:20:30 +0100

    Salvata lingua selezionata in variabile di Chrome

--- azioni.js
+++ azioni.js
@@ -1,5 +1,7 @@
+
 var languages = ["it-it","es-es","en-uk","fr-fr"];
 var images = [];
+var choice;
 for (var i = 0; i < languages.length; i++) {
 	var splitted = languages[i].split("-");
 	images.push("<img src=" + chrome.runtime.getURL("images/"+splitted[1]+".png") + " >");
@@ -17,38 +19,40 @@ $("#before-appbar").append(
 "</div>" +
 "</div>"
 );
-
 for(var i = 0; i < languages.length; i++) {
 $("#row").append(
 	"<div class='divTableCell' id='"+ languages[i] +"'>" + images[i] + "</div>");
 }
-
 for (var i = 0; i < languages.length; i++) {
-	$("#" + languages[i]).append(
-		"<div class='radio'>" + 
-		"<label><input type='radio' name='language' id='itradio' value='"+ languages[i] +"'></input>" + 
-		"</div>"
+		$("#" + languages[i]).append(
+			"<div class='radio'>" + 
+			"<label><input type='radio' name='language' value='"+ languages[i] +"' id='"+ languages[i]+ "radio" +"'></input>" + 
+			"</div>"
 		);
-}
-
+	}
 var radios = document.forms["formA"].elements["language"];
 for(var i = 0, max = radios.length; i < max; i++) {
     radios[i].onclick = function() {
        var value = this.value;
+       chrome.storage.sync.set({"checked": value});
        var splittedValue = value.split("-");
        var url = window.location.href;
-       var q = url.match(/q=(.+)&oq=/);
+       //var q = url.match(/q=(.+)&oq=/);
        //var query = q[1];
-       //alert(query);
        if(url.includes("search?sxsrf")) {
 	       var newUrl = url.replace("search?sxsrf","search?hl=" + splittedValue[0] + "&gl=" + splittedValue[1] +"&sxsrf");
 	       window.location = newUrl;
 	   } else if (url.includes("search?hl=")) {
 	   		var newUrl = url.replace(/search\?hl=(.+)&gl=(.+)&sxsrf/,"search?hl=" + splittedValue[0] + "&gl=" + splittedValue[1] +"&sxsrf");
 	   		window.location = newUrl;
-	   }
+	   }	
     }
 }
+
+chrome.storage.sync.get(["checked"],function(result){
+	$("#"+result.checked+"radio").prop("checked", true);
+});
+
 
 
 
