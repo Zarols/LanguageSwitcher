1e3d2959003dab51178723552e2a8c7670021e13
Zarols <giovanni_zarola@libero.it>
2020-05-09 12:26:17 +0200

    Aggiunte altre lingue pulsante Wikipedia

--- azioni.js
+++ azioni.js
@@ -1,5 +1,4 @@
-
-var languages = ["it-it","es-es","en-uk","fr-fr"];
+var languages = ["it-it","es-es","en-uk","fr-fr","de-de","zh-cn","el-gr"];
 var images = [];
 var choice;
 for (var i = 0; i < languages.length; i++) {
@@ -10,25 +9,34 @@ for (var i = 0; i < languages.length; i++) {
 $("#before-appbar").append(
 "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>" +
 "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>" +
+"<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css'>"+
 "<div class='divTable' id='divTable'>" +
-"<div class'divTableBody' style='border:1px solid #E0E0E0; border-radius:10px;' id='divTableBody'>" +
+"<div class'divTableBody' style='border:1px solid #E0E0E0; border-radius:10px; padding:8px;' id='divTableBody'>" +
 "<form name='formA'>" +
 "<div class='divTableRow' id='row'>" +
+"<div class='divTableRow' id='row'>" +
 "<div class='divTableCell'><font size='3'><b>Language:</b></font></div>" +
+"</div>" +
+"<div class='divTableRow' id='row'>" +
+"<button type='button' class='btn btn-primary' id='wiki'>Wikipedia &nbsp <i class='fa fa-search'></i></button>" +
+"</div>" +
 "</form>" +
 "</div>" +
 "</div>" +
 "</div>"
 );
 
+$("#wiki").click(function(e) {
+    e.preventDefault();
+    alert("not implemented");
+});
 
-
-for(var i = 0; i < languages.length; i++) {
+for(var i = 0; i < 4; i++) {
 $("#row").append(
 	"<div class='divTableCell' id='"+ languages[i] +"'>" + images[i] + "</div>");
 }
 
-for (var i = 0; i < languages.length; i++) {
+for (var i = 0; i < 4; i++) {
 		$("#" + languages[i]).append(
 			"<div class='radio'>" + 
 			"<label><input type='radio' name='language' value='"+ languages[i] +"' id='"+ languages[i]+ "radio" +"'></input>" + 
@@ -45,24 +53,39 @@ $("#dropDown").append(
         "<button class='dropbtn' id='bottone'>Other&nbsp;" + 
         "<i class='fa fa-angle-double-down'></i>" +
         "</button>" +
-        "<div id='myDropdown' class='dropdown-content'>" +
-            "<a id='menu' href='#'>"+ 							
-            	images[0] + 
-		 	"</a>" +
-			 "<a id='menu' href='#'>"+ 							
-		 		images[0] + 
-		 	"</a>" +
-            "<a id='menu' href='#'>"+ 							
-            	images[0] + 
-			"</a>" +
-        "</div>" +
- "</div>"
- );
+        "<div id='myDropdown' class='dropdown-content'></div>"
+        );
+
+for(var i = 4; i < languages.length; i++) {
+	$("#myDropdown").append(
+		"<a id='" + languages[i] + "' >"+ 							
+			images[i] + 
+		"</a>" 
+	);
+}
+			 
 
 $("#bottone").click(function(e) {
     e.preventDefault();
     document.getElementById("myDropdown").classList.toggle("show");
 });
+
+for(var i = 4; i < languages.length; i++) {
+	$("#" + languages[i]).click(function(e){
+		var value = this.id;
+		var splittedValue = value.split("-");
+       	var url = window.location.href;
+       	if(url.includes("search?sxsrf")) {
+	       var newUrl = url.replace("search?sxsrf","search?hl=" + splittedValue[0] + "&gl=" + splittedValue[1] +"&sxsrf");
+	       chrome.storage.sync.set({"checked": value});
+	       window.location = newUrl;
+	    } else if (url.includes("search?hl=")) {
+	   	   var newUrl = url.replace(/search\?hl=(.+)&gl=(.+)&sxsrf/,"search?hl=" + splittedValue[0] + "&gl=" + splittedValue[1] +"&sxsrf");
+	   	   chrome.storage.sync.set({"checked": value});
+	   	   window.location = newUrl;
+	   }	
+	});
+}
 
 var radios = document.forms["formA"].elements["language"];
 for(var i = 0, max = radios.length; i < max; i++) {
@@ -71,8 +94,6 @@ for(var i = 0, max = radios.length; i < max; i++) {
        chrome.storage.sync.set({"checked": value});
        var splittedValue = value.split("-");
        var url = window.location.href;
-       //var q = url.match(/q=(.+)&oq=/);
-       //var query = q[1];
        if(url.includes("search?sxsrf")) {
 	       var newUrl = url.replace("search?sxsrf","search?hl=" + splittedValue[0] + "&gl=" + splittedValue[1] +"&sxsrf");
 	       window.location = newUrl;
@@ -86,7 +107,14 @@ for(var i = 0, max = radios.length; i < max; i++) {
 var control  = window.location.href;
 if(control.includes("search?hl=")) {
 	chrome.storage.sync.get(["checked"],function(result){
-		$("#"+result.checked+"radio").prop("checked", true);
+		var radios = document.forms["formA"].elements["language"];
+		for(var i = 0; i < radios.length; i++) {
+			if(result.checked == radios[i].value) {
+				$("#"+result.checked+"radio").prop("checked", true);
+			} else {
+				images[0] = images[4];
+			}
+		}
 	});
 } else {
 	$("#it-itradio").prop("checked",true);
